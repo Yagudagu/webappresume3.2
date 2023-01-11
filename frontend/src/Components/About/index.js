0e07700e1dd93eff78d161f091f17d7dd3c84bbb
Jacob Martin <Jacobhmartian@gmail.com>
2023-01-11 11:22:41 -0700

    updated about

--- frontend/src/Components/About/index.js
+++ frontend/src/Components/About/index.js
@@ -5,18 +5,19 @@ import { Wrapper, Content } from "./About.styles";
 const About = (props) => (
   <Wrapper id={props.id}>
     <p id="first">
-      I love building and fixing applications. I currently work as a Tier 3
+      I love building and fixing applications. I'm currently a Senior Customer
+      Support Engineer for Oracle Cloud Infrastructure. Before that I was a
       Technical Support Engineer for Azure App Services, Azure Functions, and
-      Azure Static Web Apps. I provide solutions and fix issues in languages
-      including node, python, C#, Java, and PHP. I also have experience as a
-      Team Lead, supporting Microsoft Teams.
+      Azure Static Web Apps. I have a lot of experience with infrastructure and
+      networking. I mostly code in JS/TS and Python, but have worked with Java
+      and C# in the past.
     </p>
 
     <p id="second">
-      I've worked at my current company for about 2 years. Before that, I lived
-      and worked for 3 and a half years in Democratic Republic of Congo. I
-      worked as a teacher, and accompanied various organizations that were
-      working for peace and development around the country.
+      I've worked in technology for almost 4 years. Before that, I lived and
+      worked for 3 and a half years in Democratic Republic of Congo. I worked as
+      a teacher, and accompanied various organizations that were working for
+      peace and development around the country.
     </p>
   </Wrapper>
 );
