edd7d04911323c2aa518aa9874b9ada80621a653
yagudagu <jacob.h.martin@oracle.com>
2024-03-29 14:48:56 -0600

    fixed resume and hover

--- frontend/src/Components/Projects/index.js
+++ frontend/src/Components/Projects/index.js
@@ -5,7 +5,7 @@ const Projects = (props) => (
     <Wrapper id={props.id}>
       <Content>
         <a
-          href="https://objectstorage.us-phoenix-1.oraclecloud.com/p/8mE5fZ7Nk7GCgv3dO8CDTtFxbgvvXINUavIiG7z-GF8gHDZzzgrKuovlHiZ6XeG4/n/axwwwwm69s0m/b/jacobs-bucket/o/Jacob%20Martin%20Resume%202023%20Nov.pdf"
+          href="https://axwwwwm69s0m.objectstorage.us-phoenix-1.oci.customer-oci.com/p/gw2cz16TBjJ4glWmo6kWLuhSwm_ZYQT2byxWg-dRfjoIlc0V0W6CEzp_jyUeXIsl/n/axwwwwm69s0m/b/jacobs-bucket/o/Jacob%20Martin%20Resume%202024%20Mar.docx.pdf"
           target="_blank"
           rel="noopener noreferrer"
           className="rez_button"
